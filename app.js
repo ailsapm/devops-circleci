1b24516ab12fa23758eaa570d039d80db787a713
Ailsa Prideaux-Mooney <x23283700@student.ncirl.ie>
2025-02-17 20:42:08 +0000

    Deployment Test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var mathRouter = require('./routes/math');
 
 var app = express();
 
-// view engine setup
+// view engine setup - Deployment Test
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
